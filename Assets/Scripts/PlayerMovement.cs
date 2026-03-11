6d262dc5520317130b89944fbbc743c74d3da303
JKS42 <kian.jsmit@gmail.com>
2026-03-11 23:30:37 +0200

    Added movement to the player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,207 @@
+using UnityEngine.InputSystem;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Movement")]
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    [SerializeField] private float moveSpeed = 5f;
+
+    [SerializeField] private float sprintMultiplier = 1.5f;
+[Header("References")]
+
+    [SerializeField] private CharacterController characterController;
+
+    [SerializeField] private InputActionAsset inputActions;
+
+    [SerializeField] private Camera playerCamera;
+
+    private InputActionMap playerActionMap;
+
+    private InputAction moveAction;
+
+    private InputAction sprintAction;
+
+    private InputAction jumpAction;
+
+    private InputAction crouchAction;
+
+    private InputAction attackAction;
+
+    private Vector3 velocity;
+
+    private float gravity = -9.81f;
+
+    [SerializeField] private float jumpForce = 5f;
+
+    [SerializeField] private float crouchHeight = 0.6f;
+
+    private float normalHeight = 2f;
+
+    private void Awake()
+
+    {
+
+        // Get character controller if not assigned
+
+        if (characterController == null)
+
+            characterController = GetComponent<CharacterController>();
+
+        // Get camera if not assigned
+
+        if (playerCamera == null)
+
+            playerCamera = GetComponentInChildren<Camera>();
+
+        // Setup input system
+
+        if (inputActions == null)
+
+            inputActions = Resources.Load<InputActionAsset>("InputSystem_Actions");
+
+        playerActionMap = inputActions.FindActionMap("Player");
+
+        moveAction = playerActionMap.FindAction("Move");
+
+        sprintAction = playerActionMap.FindAction("Sprint");
+
+        jumpAction = playerActionMap.FindAction("Jump");
+
+        crouchAction = playerActionMap.FindAction("Crouch");
+
+        attackAction = playerActionMap.FindAction("Attack");
+
+        // Store normal height
+
+        normalHeight = characterController.height;
+
+    }
+
+    private void OnEnable()
+
+    {
+
+        playerActionMap.Enable();
+
+        // Subscribe to input actions
+
+        jumpAction.performed += OnJump;
+
+        crouchAction.performed += OnCrouch;
+
+        crouchAction.canceled += OnStopCrouch;
+
+        attackAction.performed += OnAttack;
+
+    }
+
+    private void OnDisable()
+
+    {
+
+        playerActionMap.Disable();
+
+        jumpAction.performed -= OnJump;
+
+        crouchAction.performed -= OnCrouch;
+
+        crouchAction.canceled -= OnStopCrouch;
+
+        attackAction.performed -= OnAttack;
+
+    }
+
+    private void Update()
+
+    {
+
+        HandleMovement();
+
+        ApplyGravity();
+
+        characterController.Move(velocity * Time.deltaTime);
     }
+    private void HandleMovement()
+
+    {
+
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+
+        bool isSprinting = sprintAction.IsPressed();
+
+        // Convert 2D input to 3D movement
+
+        Vector3 moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
+
+        // Apply sprint multiplier
+
+        float currentSpeed = moveSpeed * (isSprinting ? sprintMultiplier : 1f);
+
+        // Update horizontal velocity
+
+        velocity.x = moveDirection.x * currentSpeed;
+
+        velocity.z = moveDirection.z * currentSpeed;
+
+    }
+
+    private void ApplyGravity()
+
+    {
+
+        if (characterController.isGrounded && velocity.y < 0)
+
+        {
+
+            velocity.y = -2f; // Small negative value to keep grounded
+
+        }
+
+        else
+
+        {
+
+            velocity.y += gravity * Time.deltaTime;
+
+        }
+
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+
+    {
+
+        if (characterController.isGrounded)
+
+        {
+
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+
+        }
+
+    }
+
+    private void OnCrouch(InputAction.CallbackContext context)
+
+    {
+
+        characterController.height = crouchHeight;
+
+    }
+
+    private void OnStopCrouch(InputAction.CallbackContext context)
+
+    {
+
+        characterController.height = normalHeight;
+
+    }
+private void OnAttack(InputAction.CallbackContext context)
+
+    {
+
+        
+
+    }
 }
