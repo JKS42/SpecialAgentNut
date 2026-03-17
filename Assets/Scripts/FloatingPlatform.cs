fb5ec5eaa5e5dd5d4825b41c1aeb8af03edbdc40
JKS42 <kian.jsmit@gmail.com>
2026-03-18 00:51:39 +0200

    Adding the moving platform
    -Added the script to the floating platform
    -Added two points for the platform to float between
    -Just need to figure out to make the player move at the same speed and direction as the platform

--- Assets/Scripts/FloatingPlatform.cs
+++ Assets/Scripts/FloatingPlatform.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class FloatingPlatform : MonoBehaviour
 {
+    [SerializeField] private float height;
+    [SerializeField] private Transform[] platformPoints;
+    [SerializeField] private float lerpSpeed = 1f;
+
+    private float t = 0f;
+    private int currentIndex = 0;
+    private int targetIndex = 1;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,21 @@ public class FloatingPlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        MovePlatform();
+    }
+    private void MovePlatform()
+    {
+        if (platformPoints.Length < 2) return;
+
+        t += Time.deltaTime * lerpSpeed;
+        transform.position = Vector3.Lerp(platformPoints[currentIndex].position, platformPoints[targetIndex].position, t);
+
+        if (t >= 1f)
+        {
+            t = 0f;
+            int temp = currentIndex;
+            currentIndex = targetIndex;
+            targetIndex = temp;
+        }
     }
 }
