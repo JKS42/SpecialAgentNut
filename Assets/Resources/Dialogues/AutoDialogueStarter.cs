7c482cf50a278e53306e1a62c6b782be725f15a7
LiamFullard <liamfullard23@gmail.com>
2026-03-30 11:41:45 +0200

    Player UI added
    - lives icon
    - UIManager script
    - created player UI canvas with lives and points counter

--- Assets/Resources/Dialogues/AutoDialogueStarter.cs
+++ Assets/Resources/Dialogues/AutoDialogueStarter.cs
@@ -5,6 +5,7 @@ public class AutoDialogueStarter : MonoBehaviour
     public DialogueManager dialogueManager;
     public string fileName;
 
+    // displays dialogue automatically when the scene starts
     void Start()
     {
 
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    [Header("Lives")]
+    public Image[] hearts; 
+    public int maxLives = 3;
+    private int currentLives;
+
+    [Header("Score")]
+    public TMP_Text scoreText; 
+    private int score = 0;
+
+    void Start()
+    {
+        currentLives = maxLives;
+        UpdateLivesUI();
+        UpdateScoreUI();
+    }
+
+    // Call this to reduce a life
+    public void LoseLife()
+    {
+        currentLives--;
+        if (currentLives < 0) currentLives = 0;
+        UpdateLivesUI();
+    }
+
+    // Call this to add score
+    public void AddScore(int amount)
+    {
+        score += amount;
+        UpdateScoreUI();
+    }
+
+    void UpdateLivesUI()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            hearts[i].enabled = i < currentLives;
+        }
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
