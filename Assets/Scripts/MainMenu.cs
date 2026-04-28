541902bb656b816e3eff589badf416078d6b7f30
LiamFullard <liamfullard23@gmail.com>
2026-04-28 10:33:43 +0200

    Added Level Select feature

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public GameObject howToPlayPanel;
+    public GameObject levelSelectPanel;
 
     public void PlayGame()
     {
@@ -23,5 +24,24 @@ public class MainMenu : MonoBehaviour
     public void HideHowToPlay()
     {
         howToPlayPanel.SetActive(false);
+    }
+
+    public void ShowLevelSelect()
+    {
+        levelSelectPanel.SetActive(true);
+    }
+
+    public void HideLevelSelect()
+    {
+        levelSelectPanel.SetActive(false);
+    }
+
+    public void AdvancedLevel()
+    {
+        SceneManager.LoadScene("Advanced_Level");
+    }
+    public void ExpertLevel()
+    {
+        SceneManager.LoadScene("Expert_Level");
     }
 }
