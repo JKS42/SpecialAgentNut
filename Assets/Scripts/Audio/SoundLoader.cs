3d7c9b0082ca0fa338f8da681ccb7f07062fa554
LiamFullard <liamfullard23@gmail.com>
2026-06-15 12:25:45 +0200

    Small change

--- Assets/Scripts/Audio/SoundLoader.cs
+++ Assets/Scripts/Audio/SoundLoader.cs
@@ -5,11 +5,15 @@ public class SoundLoader : MonoBehaviour
     public AudioClip footstep;
     public AudioClip enemyAttack;
     public AudioClip platformMove;
+    public AudioClip pickup;
+    public AudioClip death;
 
     private void Start()
     {
         SFXManager.Instance.AddSound("Footstep", footstep);
         SFXManager.Instance.AddSound("EnemyAttack", enemyAttack);
         SFXManager.Instance.AddSound("PlatformMove", platformMove);
+        SFXManager.Instance.AddSound("Pickup", pickup);
+        SFXManager.Instance.AddSound("Death", death);
     }
 }
